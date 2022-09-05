9083a9b1db671ec23405d34a9476890361fb6c5a
maknetaRo <makneta@gmail.com>
2022-09-05 21:31:49 +0200

    add tests to TextInput

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -6,10 +6,7 @@
     <div class="flex flex-nowrap flex-1 h-full text-base font-light">
       <div class="relative flex items-center flex-1 h-full pr-3">
         <label for="" class="absolute left-0 -top-10">Role </label>
-        <text-input
-          placeholder="Software engineer"
-          @handle-input="updateRole"
-        />
+        <text-input v-model="role" placeholder="Software engineer" />
       </div>
       <span
         class="flex items-center h-full px-3 border-l border-r border-brand-gray-3 bg-brand-gray-2"
@@ -17,7 +14,7 @@
       >
       <div class="relative flex items-center flex-1 h-full pl-3">
         <label for="" class="absolute left-0 -top-10">Where?</label>
-        <text-input placeholder="Los Angeles" @handle-input="updateLocation" />
+        <text-input v-model="location" placeholder="Los Angeles" />
       </div>
     </div>
     <action-button text="Search" type="secondary" class="rounded-r-3xl" />
--- src/components/Shared/TextInput.vue
+++ src/components/Shared/TextInput.vue
@@ -1,7 +1,7 @@
 <template>
   <input
     type="text"
-    :value="value"
+    :value="modelValue"
     :placeholder="placeholder"
     class="w-full text-lg font-normal focus:outline-none"
     @input="handleInput"
@@ -14,19 +14,19 @@ export default {
   props: {
     placeholder: {
       type: String,
-      requierd: false,
+      required: false,
       default: "",
     },
+    modelValue: {
+      type: String,
+      required: true,
+    },
   },
-  data() {
-    return {
-      value: "",
-    };
-  },
+  emits: ["update:modelValue"],
+
   methods: {
     handleInput($event) {
-      this.value = $event.target.value;
-      this.$emit("handleInput", this.value);
+      this.$emit("update:modelValue", $event.target.value);
     },
   },
 };
